27187bf721e1f32fa341f334696656e450949b8e
IrinaT <itorosenko@mail.ru>
2020-05-11 03:54:02 +0400

    commit last

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
     cin>>y;
     cout << "sum x + y = "<< x + y << endl;
     cout << "diff x - y = "<< x - y << endl;
-    cout << "sum x + y = "<< x + y << endl;
+    cout << "mult x * y = "<< x * y << endl;
     cout << "div x / y = "<< x / y << endl;
     return 0;
 }
