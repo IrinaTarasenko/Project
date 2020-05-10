edd0da6d83d101a6d9a08e70cdf2c4f1e9969e6f
IrinaT <itorosenko@mail.ru>
2020-05-11 03:28:26 +0400

    commit 6.2

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main()
     int x,y;
     cin>>x;
     cin>>y;
+    cout << "sum x + y = "<< x + y << endl;
     cout << "diff x - y = "<< x - y << endl;
+    cout << "div x / y = "<< x / y << endl;
     return 0;
 }
